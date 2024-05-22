f213685d2aca7cfc897b12f00a4ebf7193727dbb
guifterra <guilhermeterra391@gmail.com>
2024-05-22 02:12:09 -0300

    implementacao de validacao no cadastro de usuario

--- src/main/java/br/com/fatex/backend_fatex/entities/Usuario.java
+++ src/main/java/br/com/fatex/backend_fatex/entities/Usuario.java
@@ -10,6 +10,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,16 +25,23 @@ import lombok.Setter;
 public class Usuario {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USU_ID")
     private int usuId;
 
+    @Email(regexp = ".+@fatec\\.sp\\.gov\\.br", message = "Email deve terminar em @fatec.sp.gov.br")
+    @Size(min = 6, message = "Email deve ter no mínimo 6 caracteres")
+    @NotBlank(message = "Email é obrigatório")
     @Column(name = "USU_EMAIL")
     private String usuEmail;
 
+    @Pattern(regexp = "(?=.*[A-Z])(?=.*[@#$%^&+=-_]).{6,}", message = "Senha deve conter 1 caracter maiúsculo, tamanho mínimo de 6 e um caracter especial")
+    @NotBlank(message = "Senha é obrigatória")
     @Column(name = "USU_SENHA")
     private String usuSenha;
 
+    @Size(min = 6, message = "Nome deve ter no mínimo 6 caracteres")
+    @NotBlank(message = "Nome é obrigatório")
     @Column(name = "USU_NOME")
     private String usuNome;
 
@@ -38,10 +49,12 @@ public class Usuario {
     @Temporal(TemporalType.DATE)
     private Date usuDtNascimento;
 
-    @Column(name = "USU_GENERO")
     @Enumerated(EnumType.STRING)
+    @Column(name = "USU_GENERO")
     private Genero usuGenero;
 
+    @Pattern(regexp = "\\d{11}", message = "CPF deve ter 11 caracteres e conter apenas números")
+    @NotBlank(message = "CPF é obrigatório")
     @Column(name = "USU_CPF", unique = true)
     private String usuCpf;
 }
